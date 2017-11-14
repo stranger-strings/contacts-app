de8b1c8e718909b049537e743a8a474c68291c54
Peter Jang <peter.x.jang@gmail.com>
2017-11-13 21:10:15 -0600

    Add seeds file to create contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+x = Contact.new(first_name: "Bob", last_name: "Charlie", email: "bob@bob.com", phone_number: "432423432423")
+x.save
+
+Contact.create(first_name: "Saron", last_name: "Yitbarek", email: "saron@email.com", phone_number: "3242432")
+Contact.create(first_name: "Majora", last_name: "Carter", email: "majora@email.com", phone_number: "435435435")
