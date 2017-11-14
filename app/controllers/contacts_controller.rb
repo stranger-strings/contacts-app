6dd5f3dc64f4d5370363aa25c263a7ee176ce061
Peter Jang <peter.x.jang@gmail.com>
2017-11-13 20:57:36 -0600

    Add route to show all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,9 @@ class ContactsController < ApplicationController
     contact = Contact.first
     render json: contact.as_json
   end
+
+  def show_all_method
+    contacts = Contact.all
+    render json: contacts.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/show_one_contact_url" => "contacts#show_one_method"
+  get "/show_all_contacts_url" => "contacts#show_all_method"
 end
