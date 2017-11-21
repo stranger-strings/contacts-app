ae94f774c06d2eb84a84b6dc45882621e38e2de3
Peter Jang <peter.x.jang@gmail.com>
2017-11-20 18:13:10 -0600

    Add custom model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,21 @@
 class Contact < ApplicationRecord
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+  end
+
+  def as_json
+    {
+      id: id,
+      first_name: first_name,
+      last_name: last_name,
+      full_name: full_name,
+      email: email,
+      phone_number: phone_number,
+      japan_phone_number: japan_phone_number,
+    }
+  end
 end
